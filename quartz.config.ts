b8000019ca2a8a512659517718db8cfd2f4770fd
Mateus Furquim <mfurquimdev@gmail.com>
2024-01-04 11:01:50 -0300

    Configure and add content

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,5 @@
-import { QuartzConfig } from "./quartz/cfg"
-import * as Plugin from "./quartz/plugins"
+import { QuartzConfig } from "./quartz/cfg";
+import * as Plugin from "./quartz/plugins";
 
 /**
  * Quartz 4.0 Configuration
@@ -7,84 +7,91 @@ import * as Plugin from "./quartz/plugins"
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
 const config: QuartzConfig = {
-  configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
-    theme: {
-      cdnCaching: true,
-      typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
-      },
-      colors: {
-        lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+    configuration: {
+        pageTitle: "MFurquim Garden",
+        enableSPA: true,
+        enablePopovers: true,
+        analytics: {
+            provider: "plausible",
         },
-        darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-      },
-    },
-  },
-  plugins: {
-    transformers: [
-      Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
-      }),
-      Plugin.Latex({ renderEngine: "katex" }),
-      Plugin.SyntaxHighlighting({
+        locale: "en-US",
+        baseUrl: "mfurquim.dev",
+        ignorePatterns: [
+            "private",
+            "templates",
+            ".obsidian",
+            "!(PublicMedia)**/!(*.md)",
+            "!(*.md)",
+        ],
+        defaultDateType: "created",
         theme: {
-          light: "github-light",
-          dark: "github-dark",
+            fontOrigin: "googleFonts",
+            cdnCaching: true,
+            typography: {
+                header: "Schibsted Grotesk",
+                body: "Source Sans Pro",
+                code: "IBM Plex Mono",
+            },
+            colors: {
+                lightMode: { // https://gitlab.com/yorickpeterse/vim-paper/-/tree/master?ref_type=heads
+                    light: "#f2eede",
+                    lightgray: "#d8d5c7", // Graph border and search background
+                    gray: "#777777",
+                    darkgray: "#242933", // Normal text
+                    dark: "#000000",
+                    secondary: "#216609",
+                    tertiary: "#b58900",
+                    highlight: "rgba(247, 243, 227, 0.15)",
+                },
+                darkMode: { // https://www.nordtheme.com
+                    light: "#242933",
+                    lightgray: "#3B4252",
+                    gray: "#6f7d98",
+                    darkgray: "#D8DEE9",
+                    dark: "#ECEFF4",
+                    secondary: "#81A1C1",
+                    tertiary: "#6f7d98",
+                    highlight: "rgba(180, 142, 173, 0.15)",
+                },
+            },
         },
-        keepBackground: false,
-      }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Description(),
-    ],
-    filters: [Plugin.RemoveDrafts()],
-    emitters: [
-      Plugin.AliasRedirects(),
-      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
-      Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
-      Plugin.ContentIndex({
-        enableSiteMap: true,
-        enableRSS: true,
-      }),
-      Plugin.Assets(),
-      Plugin.Static(),
-      Plugin.NotFoundPage(),
-    ],
-  },
-}
+    },
+    plugins: {
+        transformers: [
+            Plugin.FrontMatter(),
+            Plugin.CreatedModifiedDate({
+                priority: ["frontmatter", "filesystem"],
+            }),
+            Plugin.SyntaxHighlighting({
+                theme: {
+                    light: "github-light",
+                    dark: "github-dark",
+                },
+                keepBackground: false,
+            }),
+            Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+            Plugin.GitHubFlavoredMarkdown(),
+            Plugin.TableOfContents(),
+            Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+            Plugin.Description(),
+            Plugin.Latex({ renderEngine: "katex" }),
+        ],
+        filters: [Plugin.RemoveDrafts()],
+        emitters: [
+            Plugin.AliasRedirects(),
+            Plugin.ComponentResources(),
+            Plugin.ContentPage(),
+            Plugin.FolderPage(),
+            Plugin.TagPage(),
+            Plugin.ContentIndex({
+                enableSiteMap: true,
+                enableRSS: true,
+            }),
+            Plugin.Assets(),
+            Plugin.Static(),
+            Plugin.NotFoundPage(),
+        ],
+    },
+};
 
-export default config
+export default config;
